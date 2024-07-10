a8dbca9800b1e1e01ef0ffc8a51aae139304b755
Roman <shelepov-rmn@mail.ru>
2024-07-10 13:20:16 +0300

    add: MVC structure_1

---
+++ src/js/model.js
@@ -0,0 +1,27 @@
+// Текущий рецепт, состояние
+export const state = { recipe: {} };
+
+export const loadRecipe = async function () {
+  try {
+    // Загрузка рецепта
+    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    const recipe = data.data.recipe;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (error) {
+    alert(error);
+  }
+};
