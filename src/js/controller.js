0d4c18f23b73abf92fae3a3dd92e8c9d9ed97239
Roman <shelepov-rmn@mail.ru>
2024-09-17 00:50:44 +0300

    add: config and helpers files

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,32 +7,24 @@ import "regenerator-runtime/runtime";
 
 const recipeContainer = document.querySelector(".recipe");
 
-// const timeout = function (s) {
-//   return new Promise(function (_, reject) {
-//     setTimeout(function () {
-//       reject(new Error(`Request took too long! Timeout after ${s} second`));
-//     }, s * 1000);
-//   });
-// };
-
 const controlRecipes = async function () {
-  try {
-    // Защита от отсутствия выбранного рецепта (хэша)
-    const id = window.location.hash.slice(1);
-    if (!id) return;
-    recipeView.renderSpinner();
+    try {
+        // Защита от отсутствия выбранного рецепта (хэша)
+        const id = window.location.hash.slice(1);
+        if (!id) return;
+        recipeView.renderSpinner();
 
-    // Загрузка рецепта
-    await model.loadRecipe(id);
+        // Загрузка рецепта
+        await model.loadRecipe(id);
 
-    // 2. Отрисовка рецепта
-    recipeView.render(model.state.recipe);
-  } catch (err) {
-    alert(err);
-  }
+        // 2. Отрисовка рецепта
+        recipeView.render(model.state.recipe);
+    } catch (err) {
+        alert(err);
+    }
 };
 
 // Загрузка рецепта при смене ссылки \ загрузке страницы
 ["load", "hashchange"].forEach(function (EvElem) {
-  window.addEventListener(EvElem, controlRecipes);
+    window.addEventListener(EvElem, controlRecipes);
 });
--- src/js/model.js
+++ src/js/model.js
@@ -1,27 +1,28 @@
+import { API_URL } from "./config.js";
+import { getJSON } from "./helpers.js";
+
 // Текущий рецепт, состояние
 export const state = { recipe: {} };
 
 export const loadRecipe = async function () {
-  try {
-    // Загрузка рецепта
-    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
-    const data = await res.json();
+    try {
+        // Загрузка рецепта
+        const data = await getJSON(`${API_URL}/${id}`);
 
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    const recipe = data.data.recipe;
-    state.recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-    console.log(state.recipe);
-  } catch (error) {
-    alert(error);
-  }
+        const recipe = data.data.recipe;
+        state.recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients,
+        };
+        console.log(state.recipe);
+    } catch (err) {
+        // Обработка ошибки из getJSON()
+        console.error(`${err}`);
+    }
 };
